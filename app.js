31694d4b4e1e012f625d14780f8ac411a474edbd
Patricia-Bekyere <91859038+Patricia-Bekyere@users.noreply.github.com>
2021-12-14 13:31:08 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// assigning my age a number
+
+let myAge = 24;
+
+// creating a variable name early years
+
+let earlyYears = 2;
+// multiplying early years by 10.5
+earlyYears *= 10.5;
+
+// subtracting 2 from my age and assigning it to later years
+
+let laterYears = myAge - 2;
+
+// multiply later years by 4
+
+laterYears =laterYears * 4;
+
+// printing early and later years to the screen
+console.log(earlyYears);
+console.log(laterYears);
+
+// storing both in one variable
+let myAgeInDogYears = earlyYears + laterYears;
+
+// storing my name in a variable
+let myName = "patricia bekyere".toUpperCase();
+
+// printing my name and age to the console using interpolation
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
